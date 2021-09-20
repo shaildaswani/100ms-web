c0535beb8c50893d41836bdddaf0252089fdc707
Shail Daswani <shail.daswani@gmail.com>
2021-09-20 12:22:31 +0530

    Changes for Sarva Demo

---
+++ src/common/Constants.js
@@ -0,0 +1,32 @@
+export const defaultAudioList = [
+  {
+    name: "Sarva1",
+    id: "audio1",
+    metadata: {
+      description: "Artist1",
+    },
+    url: "https://bc-public-static-assets.s3.ap-south-1.amazonaws.com/webapp/playlist/Sarva1.mp3",
+    type: "audio",
+  }
+];
+
+export const defaultVideoList = [
+  {
+    name: "01_Beginners_Flow_720",
+    id: "video1",
+    metadata: {
+      description: "Artist1",
+    },
+    url: "https://bc-public-static-assets.s3.ap-south-1.amazonaws.com/webapp/playlist/01_Beginners_Flow_720.mp4",
+    type: "video",
+  },
+  {
+    name: "Yogic_Cardio_Flow_01_Lower_Body-720p",
+    id: "video2",
+    metadata: {
+      description: "Artist2",
+    },
+    url: "https://bc-public-static-assets.s3.ap-south-1.amazonaws.com/webapp/playlist/Yogic_Cardio_Flow_01_Lower_body-720p.mp4",
+    type: "video",
+  }
+];
